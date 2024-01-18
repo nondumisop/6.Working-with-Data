0805a8fd13056182088d31c43d86e2f744f2516d
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-18 06:20:56 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("MV Boli", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(763, 275)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(176, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Start Here"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.WhatsApp_Image_2024_01_16_at_21_06_23
+        ClientSize = New Size(1773, 840)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "BirthYear"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMessage As String
+
+        Dim BirthYear As Integer
+        YourMessage = "Happy Birthday! How Old are you now"
+
+        MsgBox(YourMessage)
+        BirthYear = Val(InputBox("enter Year of birth"))
+        MsgBox("you are now" & 2023 - BirthYear & " years old")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
+
